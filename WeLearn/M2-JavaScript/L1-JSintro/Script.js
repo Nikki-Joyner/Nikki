88e21fe849176141c136dcb70c3caeb0a5d70bfc
Nikkita Joyner <joyne004@csusm.edu>
2019-07-24 12:01:21 -0700

    -CSS lab Complete -JavaScript Intro

---
+++ WeLearn/M2-JavaScript/L1-JSintro/Script.js
@@ -0,0 +1,12 @@
+console.log("script running!");
+
+let name= 'Charlie';
+console.log('Hello '+ name + '!');
+let age =15;
+console.log('You are '+ age + ' years old!');
+if (age>=16) {
+console.log("You can drive");
+} else {
+console.log("You can't drive");
+}
+if (animal ===)
