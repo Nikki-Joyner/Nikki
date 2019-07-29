dc41c9081f54802a6269fafd6e4cd299270ee834
Nikkita Joyner <joyne004@csusm.edu>
2019-07-29 10:48:11 -0700

    AppEngine Work

---
+++ WeLearn/M3-Python/L1-Python_Intro/hello.py
@@ -0,0 +1,44 @@
+'''print("hello world")
+print("bye world")
+
+num1 =int(raw_input("Enter number #1: "))
+num2 = int(raw_input("Enter number #2: "))
+total = num1 + num2
+print("the sum is: " + str(total))
+
+
+num=int(raw_input("Enter a number: "))
+if num > 0:
+        print ("Thats a positive number!")
+elif num < 0:
+    print("Zero is neither positive nor negative")
+
+num=int(raw_input("Enter a new number: "))
+if num > 1000:
+    print("Thats over 1000")
+elif num <-100:
+    print("Its under 100")'''
+
+'''string = "hello there"
+for letter in string:
+    print(letter.upper())
+for i in range(5):
+    print(i)
+x=1
+while x<=5:
+    print(x)
+    x=x + 1'''
+
+'''my_name="Nikki"
+friend1="Alex"
+friend2="John"
+friend3="Mike"
+
+print(
+    "My name is %s and my friends are %s,%s,and %s" %
+    (my_name,friend1,friend2,friend3)
+)'''
+
+'''def greetAgent(first_name, last_name):
+    print("%s.%s %s" % (last_name, first_name, last_name))
+greetAgent('Nikkita', 'Joyner')'''
